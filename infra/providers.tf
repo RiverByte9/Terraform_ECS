5ab226b0684993b337153210c8f3b1759c22d91e
KAJAL <kajalsalgar909@gmail.com>
2025-05-26 10:52:46 +0530

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -3,6 +3,18 @@
 # random plugins
 # template plugins
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
 
 
 provider "aws" {
