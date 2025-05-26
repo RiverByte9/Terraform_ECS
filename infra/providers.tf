d3e692d4c787ed224dd751d23c62a7685a8f7281
KAJAL <kajalsalgar909@gmail.com>
2025-05-26 10:50:20 +0530

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -30,7 +30,7 @@ provider "aws" {
 # Using S3 backend
 terraform {
   backend "s3" {
-    bucket  = "334455-devops-bootcamp-state"
+    bucket  = "456-devops-bootcamp-state"
     key     = "2-tier-flask/dev-tfstate"
     region  = "ap-south-1"
     encrypt = true
