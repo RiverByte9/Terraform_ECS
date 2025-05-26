4cdd058014efaf41021aa2d3c9bcdb1e9b4d4d30
KAJAL <kajalsalgar909@gmail.com>
2025-05-26 12:08:36 +0530

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -4,14 +4,16 @@
 # template plugins
 
 terraform {
+  required_version = "1.10.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.50.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "~> 3.4.3"
     }
   template = {
      source  = "hashicorp/template"
